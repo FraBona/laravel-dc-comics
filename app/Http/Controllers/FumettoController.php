c9f0981d171f7e3e13a7d1688f4fff4a5e7938c9
FraBona <francescobonafede08@gmail.com>
2023-12-18 15:48:42 +0100

    create table

---
+++ app/Http/Controllers/FumettoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreFumettoRequest;
+use App\Http\Requests\UpdateFumettoRequest;
+use App\Models\Fumetto;
+
+class FumettoController extends Controller
+{
+ 
+}
